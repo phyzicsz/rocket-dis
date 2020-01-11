1ffd1c34445c6874480b6e64092c1b789d21b748
phyzicsz <phyzics.z@gmail.com>
2020-01-10 23:28:00 -0600

    general cleanup

--- dis-codegen-plugin/src/main/java/com/phyzicsz/dis/datamodel/api/DisClasses.java
+++ dis-codegen-plugin/src/main/java/com/phyzicsz/dis/datamodel/api/DisClasses.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 pborawski.
+ * Copyright 2019 phyzicsz.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import java.util.List;
 
 /**
  *
- * @author pborawski
+ * @author phyzicsz
  */
 @XStreamAlias("classes")
 public class DisClasses {
--- dis-codegen-plugin/src/main/java/com/phyzicsz/dis/datamodel/api/DisPrimitive.java
+++ dis-codegen-plugin/src/main/java/com/phyzicsz/dis/datamodel/api/DisPrimitive.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 pborawski.
+ * Copyright 2019 phyzicsz.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 
 /**
  *
- * @author pborawski
+ * @author phyzicsz
  */
 public class DisPrimitive {
     
